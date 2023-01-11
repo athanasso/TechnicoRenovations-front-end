29ee20893a93f6445c083992c8ec844ade398eff
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-11 15:43:10 +0200

    reformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './login-page/login-page.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+
 
 @NgModule({
   declarations: [
