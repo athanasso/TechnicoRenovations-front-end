2e8d3a75c31013469f27f11fa533e1420a2e400c
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-11 16:42:24 +0200

    initial home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './components/user/home/home.component';
 import { AboutusComponent } from './core/aboutus/aboutus.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { NgModule } from '@angular/core';
@@ -6,6 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', component: LoginPageComponent },
   { path: 'aboutus', component: AboutusComponent },
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
