c74b18b00d5607ea265e0a7b1840d4e0d6ad88bd
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-13 14:03:10 +0200

    initial login functionality

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
+import { AuthServiceService } from './../../services/auth/auth-service.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +10,43 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private router: Router) {
+  public user: any;
+  login : any;
+  message = '';
 
-  }
+  constructor(private router: Router, private service: AuthServiceService, private fb: FormBuilder) {}
+
+  userLoginForm: FormGroup = this.fb.group({
+    username: this.fb.control(""),
+    password: this.fb.control(""),
+  });
 
   goToRegister() {
     this.router.navigate(['/register']);
   }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    this.login = {
+      "username": this.userLoginForm.get("username")?.value,
+      "password": this.userLoginForm.get("password")?.value
+    };
+
+    this.service.getUser(this.userLoginForm).subscribe({
+      next: data => {
+        this.user = data;
+        console.log(this.user);
+      },
+      error: er => this.message = "Error" + er.message,
+      complete: () => this.message = "Completed..."
+    });
+
+    if (this.user && this.user.typeOfUser=="user"){
+      this.router.navigate(['user/home']);
+    }
+    if (this.user && this.user.typeOfUser=="admin"){
+      this.router.navigate(['admin/home']);
+    }
+  }
 }
