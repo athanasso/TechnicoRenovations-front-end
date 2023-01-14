03ed5b80d8ddd60a1f4c8dae15e0d80701d10751
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-14 16:17:52 +0200

    Update user-service.service.ts

--- src/app/services/user/user-service.service.ts
+++ src/app/services/user/user-service.service.ts
@@ -8,14 +8,16 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class UserServiceService {
 
+  vatNumber = 1123123;
+
   private readonly createPropertyEndPoint = 'http://localhost:8080/shop/api/user/property/create_property';
   private readonly correctPropertyAddressEndPoint = 'http://localhost:8080/shop/api/user/property/correct_property_address';
   private readonly correctPropertyTypeEndPoint = 'http://localhost:8080/shop/api/user/property/correct_property_type';
   private readonly correctPropertyYearEndPoint = 'http://localhost:8080/shop/api/user/property/correct_property_year';
   private readonly createRepairEndPoint = 'http://localhost:8080/shop/api/user/repair/create_property_repair';
-  private readonly propertiesEndPoint = 'http://localhost:8080/shop/api/user/get_properties/';
+  private readonly propertiesEndPoint = 'http://localhost:8080/shop/api/user/get_properties/'+this.vatNumber;
   private readonly changeRepairStatusEndPoint = 'http://localhost:8080/shop/api/user/repair/repair_status';
-  private readonly repairStatusEndPoint = 'http://localhost:8080/shop/api/user/get_repair_status/{vatNumber}';
+  private readonly repairStatusEndPoint = 'http://localhost:8080/shop/api/user/get_repair_status/'+this.vatNumber;
   private readonly correctOwnerUsernameEndPoint = 'http://localhost:8080/shop/api/user/owner/correct_owner_username';
   private readonly correctOwnerEmailEndPoint = 'http://localhost:8080/shop/api/user/owner/correct_owner_email';
   private readonly correctOwnerPasswordEndPoint = 'http://localhost:8080/shop/api/user/owner/correct_owner_password';
@@ -30,6 +32,17 @@ export class UserServiceService {
     .set('Content-Type', 'application/json');
 
     return this.http.get(this.propertiesEndPoint, { headers: headers })
+      .pipe(
+        retry(1),
+        catchError(error => throwError(() => 'Something is wrong...'))
+      );
+  }
+
+  getRepairs(){
+    const headers = new HttpHeaders()
+    .set('Content-Type', 'application/json');
+
+    return this.http.get(this.repairStatusEndPoint, { headers: headers })
       .pipe(
         retry(1),
         catchError(error => throwError(() => 'Something is wrong...'))
