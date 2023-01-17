30d9add3cb4f04f99cbe9c7aa2bfd0ba6757bb55
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-17 12:54:20 +0200

    added form validators

--- src/app/components/user/property-details/property-details.component.ts
+++ src/app/components/user/property-details/property-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LoggedUserService } from 'src/app/services/logged-user.service';
 import { UserService } from 'src/app/services/user/user-service.service';
 
@@ -23,11 +23,11 @@ export class PropertyDetailsComponent implements OnInit{
   constructor(private loggedUser: LoggedUserService, private fb: FormBuilder, private service: UserService){}
 
   propertyRegisterForm: FormGroup = this.fb.group({
-    propertyId: this.fb.control(""),
-    propertyAddress: this.fb.control(""),
-    yearOfConstruction: this.fb.control(""),
-    propertyType: this.fb.control(""),
-    ownerVatNumber: this.fb.control("")
+    propertyId: this.fb.control("",[Validators.required]),
+    propertyAddress: this.fb.control("",[Validators.required]),
+    yearOfConstruction: this.fb.control("",[Validators.required]),
+    propertyType: this.fb.control("",[Validators.required]),
+    ownerVatNumber: this.fb.control("",[Validators.required])
   });
 
   ngOnInit(): void {
@@ -35,24 +35,26 @@ export class PropertyDetailsComponent implements OnInit{
   }
 
   onSubmit() {
-    this.register = {
-      "propertyId": this.propertyRegisterForm.get("propertyId")?.value,
-      "propertyAddress": this.propertyRegisterForm.get("propertyAddress")?.value,
-      "yearOfConstruction": this.propertyRegisterForm.get("yearOfConstruction")?.value,
-      "propertyType": this.propertyRegisterForm.get("propertyType")?.value,
-      "ownerVatNumber": this.propertyRegisterForm.get("ownerVatNumber")?.value
-    };
+    if(this.propertyRegisterForm.valid) {
+      this.register = {
+        "propertyId": this.propertyRegisterForm.get("propertyId")?.value,
+        "propertyAddress": this.propertyRegisterForm.get("propertyAddress")?.value,
+        "yearOfConstruction": this.propertyRegisterForm.get("yearOfConstruction")?.value,
+        "propertyType": this.propertyRegisterForm.get("propertyType")?.value,
+        "ownerVatNumber": this.propertyRegisterForm.get("ownerVatNumber")?.value
+      };
 
-    if (this.loggedUser.getUser().vatNumber == this.register.ownerVatNumber){
-      this.service.createProperty(this.register).subscribe({
-        next: data => {
-          this.response = data;
-        },
-        error: er => this.message = "Error" + er.message,
-        complete: () => this.message = "Completed..."
-      });
-    } else {
-      console.log("wrong ownerVatNumber")
+      if (this.loggedUser.getUser().vatNumber == this.register.ownerVatNumber){
+        this.service.createProperty(this.register).subscribe({
+          next: data => {
+            this.response = data;
+          },
+          error: er => this.message = "Error" + er.message,
+          complete: () => this.message = "Completed..."
+        });
+      } else {
+        console.log("wrong ownerVatNumber")
+      }
     }
   }
 
