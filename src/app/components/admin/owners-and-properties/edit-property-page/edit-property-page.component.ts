09fd3165613cc0016d16aab922da3026b1b17145
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-17 13:51:59 +0200

    edit property page

---
+++ src/app/components/admin/owners-and-properties/edit-property-page/edit-property-page.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { LoggedUserService } from 'src/app/services/logged-user.service';
+import { UserService } from 'src/app/services/user/user-service.service';
+
+@Component({
+  selector: 'app-edit-property-page',
+  templateUrl: './edit-property-page.component.html'
+})
+export class EditPropertyPageComponent {
+
+  searchQuery!: string;
+  filteredResponse: any;
+  properties: any;
+
+  constructor(private service: LoggedUserService, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.properties = this.service.getProperties();
+  }
+
+  search() {
+    if(!this.searchQuery){
+      this.filteredResponse = this.properties.data;
+    } else {
+      this.filteredResponse = this.properties.data.filter((propertyId: string, yearOfConstruction: number ) => {
+        return propertyId.includes(this.searchQuery.toLowerCase())
+        || yearOfConstruction == parseInt(this.searchQuery);
+      });
+    }
+  }
+
+  deleteUser(item:any) {
+    if (confirm("Are you sure you want to delete this user?")) {
+      this.userService.deleteOwner(item).subscribe(
+        (res: any) => {
+          console.log(res);
+        },
+        (err: any) => {
+          console.log(err);
+        }
+      );
+    }
+  }
+
+  updateItem(item: any) {
+    this.userService.updatePropertyAddress({ownerVatNumber: item.propertyOwner.vatNumber, propertyId: item.propertyId, propertyAddress: item.propertyAddress}).subscribe(
+      (res: any) => {
+        console.log(res);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+    this.userService.updatePropertyYear({ownerVatNumber: item.propertyOwner.vatNumber, propertyId: item.propertyId, yearOfConstruction: item.yearOfConstruction}).subscribe(
+      (res: any) => {
+        console.log(res);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+    this.userService.updateEmail({vatNumber: item.propertyOwner.vatNumber, email: item.propertyOwner.email}).subscribe(
+      (res: any) => {
+        console.log(res);
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+  }
+}
