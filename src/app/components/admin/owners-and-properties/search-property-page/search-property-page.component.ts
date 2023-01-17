fd36d86a49df8c655b60c7fdaa707f03c2093e4d
Emmanouil Athanasopoulos <manos.athanasopoulos@yahoo.gr>
2023-01-17 13:52:10 +0200

    search property page

---
+++ src/app/components/admin/owners-and-properties/search-property-page/search-property-page.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { LoggedUserService } from 'src/app/services/logged-user.service';
+
+@Component({
+  selector: 'app-search-property-page',
+  templateUrl: './search-property-page.component.html'
+})
+export class SearchPropertyPageComponent {
+
+  searchQuery!: string;
+  filteredResponse: any;
+  properties: any;
+
+  constructor(private service: LoggedUserService) { }
+
+  ngOnInit(): void {
+    this.properties = this.service.getProperties();
+  }
+
+  search() {
+    if(!this.searchQuery){
+      this.filteredResponse = this.properties.data;
+    } else {
+      this.filteredResponse = this.properties.data.filter((property: { propertyId: string; yearOfConstruction: number; }) => {
+        return property.propertyId.toLowerCase().includes(this.searchQuery.toLowerCase())
+        || property.yearOfConstruction == parseInt(this.searchQuery);
+      });
+    }
+  }
+}
